94cd0fbe448c5fabc8e2f897de4c6efe012beb5e
Bojan Banović <113462851+bokab@users.noreply.github.com>
2023-02-12 14:21:31 +0100

    Components

---
+++ components/data.js
@@ -0,0 +1,37 @@
+const data = {
+    products: [
+        {
+            id: '1',
+            name: 'MacBook',
+            price: 1400,
+            image: 'https://picsum.photos/id/180/2400/1600',
+        },
+        {
+            id: '2',
+            name: 'Old Car',
+            price: 2400,
+            image: 'https://picsum.photos/id/111/4400/2656',
+        },
+        {
+            id: '3',
+            name: 'W Shoes',
+            price: 1000,
+            image: 'https://picsum.photos/id/21/3008/2008',
+        },
+        {
+            id: '4',
+            name: 'T-shirt',
+            price: 35,
+            image: 'https://www.rei.com/media/43314827-8954-4e1e-a32b-8fd2ce0c0b99.jpg?size=576x768'
+
+        },
+        {
+            id: '5',
+            name: 'Tablet',
+            price: 250,
+            image: 'https://www.notebookcheck.com/fileadmin/Notebooks/News/_nc3/Google_Pixel_Tablet_mit_Android_12_Konzept_Patent.jpg'
+
+        }
+    ],
+};
+export default data;
